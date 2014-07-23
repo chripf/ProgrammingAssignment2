c12f93a51fcb18caae2b60b1bbb4e87f2e9db698
Christian Pfanner <chripf@gmail.com>
2014-07-23 04:49:30 +0200

    adapted commenting

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix
+##      Creates an object wrapping a matrix and then caching it's inverse after the first calculation.
+## cacheSolve
+##      Uses the object created by makeCacheMatrix(x) to solve the matrix and uses cached data, if possible.
 
-## Write a short comment describing this function
+
+## Creates an object providing functions for storing a matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     
-    set<-function(y){
+    set<-function(y){   # Sets a new value for the matrix and resets the cached value.
         x<<-y
         i<<-NULL
     }
-    get <- function()x
+    get <- function()x  # Retrieve the wrapped matrix
     
-    setinv <- function(inv) i <<- inv
-    getinv <- function() i
+    setinv <- function(inv) i <<- inv   # Store the inverse matrix for cached access.
+    getinv <- function() i              # Retrieve the cached inverse matrix.
        
     list(set = set, get = get,
          setinv = setinv,
@@ -21,11 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Uses an object created by makeCacheMatrix(x) to either return a cached inverse matrix
+## or calculate the inverse and cache it, then returning the calculated inverse matrix.  
 
-cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    inv<-x$getinv()
+cacheSolve <- function(x, ...) {    
+    inv<-x$getinv() # Try to access the cached inverse and return it, if found.
     
     if(!is.null(inv)){
         # activate for development
@@ -33,9 +36,9 @@ cacheSolve <- function(x, ...) {
         return(inv)
     }
     
-    origMatrix <- x$get()
-    inv<- solve(origMatrix)
-    x$setinv(inv)
+    origMatrix <- x$get()   # fetch original matrix
+    inv<- solve(origMatrix) # solve it
+    x$setinv(inv)           # and cache it
     inv
     
 }
